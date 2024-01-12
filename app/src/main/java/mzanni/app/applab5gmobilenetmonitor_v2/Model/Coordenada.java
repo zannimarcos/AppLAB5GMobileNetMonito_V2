73bfe058992115775b074ce5a0a27467927b6222
mzanni <zanni.marcos@gmail.com>
2024-01-12 11:30:01 -0300

    Criação de pacotes View, Model e Monitoramento e as classes necessárias

---
+++ app/src/main/java/mzanni/app/applab5gmobilenetmonitor_v2/Model/Coordenada.java
@@ -0,0 +1,4 @@
+package mzanni.app.applab5gmobilenetmonitor_v2.Model;
+
+public class Coordenada {
+}
--- app/src/main/java/mzanni/app/applab5gmobilenetmonitor_v2/View/MainActivity.java
+++ app/src/main/java/mzanni/app/applab5gmobilenetmonitor_v2/View/MainActivity.java
@@ -1,8 +1,10 @@
-package mzanni.app.applab5gmobilenetmonitor_v2;
+package mzanni.app.applab5gmobilenetmonitor_v2.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import mzanni.app.applab5gmobilenetmonitor_v2.R;
 
 public class MainActivity extends AppCompatActivity {
 
