275214d8ab3627720966aa91d049805083f72077
mzanni <zanni.marcos@gmail.com>
2024-01-12 11:00:38 -0300

    Criação do projeto

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AppLAB5GMobileNetMonitor_V2"
+include(":app")
